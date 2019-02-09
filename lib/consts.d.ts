7135aaa36001dc491d23af92e02b0569bcfc06b6
Ali Piry <alipiry@icloud.com>
2019-02-10 02:25:57 +0330

    Auto-generate files

---
+++ lib/consts.d.ts
@@ -0,0 +1,4 @@
+export declare const gregorianMonths: Array<string>;
+export declare const jalaliMonths: Array<string>;
+export declare const gregorianWeekDays: Array<string>;
+export declare const jalaliWeekDays: Array<string>;
