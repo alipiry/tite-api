0df728eaf4c6d542249470cf324ef26837e2f75f
Ali Piry <alipiry@disroot.org>
2019-02-10 02:25:57 +0330

    Auto-generate files

---
+++ lib/tite.d.ts
@@ -0,0 +1,8 @@
+export declare const nowTime: string;
+export declare const jalaliDate: string;
+export declare const gregorianDate: string;
+export declare function getYear(): string;
+export declare function getMonth(): string;
+export declare function getDay(): string;
+export declare function getWeekDay(): string;
+export declare function getTime(): string;
